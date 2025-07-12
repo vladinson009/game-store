03df70b54c29afd89ba568afd27729387b26f839
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 00:58:01 +0200

    custom repass form validator

---
+++ src/app/shared/utils/repassValidator.ts
@@ -0,0 +1,26 @@
+import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function matchPasswordValidator(
+  passwordKey: string,
+  repassKey: string
+): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const passwordControl = group.get(passwordKey);
+    const repassControl = group.get(repassKey);
+
+    if (!passwordControl || !repassControl) {
+      return null;
+    }
+    const password = passwordControl.value;
+    const repass = repassControl.value;
+
+    if (password === repass) {
+      repassControl.setErrors(null);
+      return null;
+    } else {
+      const error = { passwordsMismatch: true };
+      repassControl.setErrors(error);
+      return error;
+    }
+  };
+}
