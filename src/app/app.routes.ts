8406f3b986fc1b0b6045158a8d3a0afcaf63c551
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 00:56:44 +0200

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { GameList } from './features/games/game-list/game-list';
 import { Home } from './layout/home/home';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'games', component: GameList },
+  { path: 'users/login', component: Login },
+  { path: 'users/register', component: Register },
 ];
