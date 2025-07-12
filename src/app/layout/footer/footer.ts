48b800af4e18383f77cb5a30ad8184692dc76609
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 00:56:10 +0200

    implemented header & footer layout

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 import { MatToolbarRow, MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
 import { CopyrightDirective } from '../../shared/directives/copyright.directive';
+import { f } from '../../../../node_modules/@angular/material/icon-module.d-COXCrhrh';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
