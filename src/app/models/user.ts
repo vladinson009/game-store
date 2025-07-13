219714f12df8045bc888feb1291531c6adb536e1
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 19:19:47 +0200

    added user interfaces

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,31 @@
 import { FormControl } from '@angular/forms';
 
-export interface LoginUserForm {
+//* Form Control Interfaces
+interface AuthFormBase {
   username: FormControl<string>;
   password: FormControl<string>;
 }
-export interface RegisterUserForm {
-  username: FormControl<string>;
+export interface LoginUserForm extends AuthFormBase {}
+export interface RegisterUserForm extends AuthFormBase {
   email: FormControl<string>;
-  password: FormControl<string>;
   repass: FormControl<string>;
+}
+
+//* User Credentials Interfaces
+interface AuthCredentialsBase {
+  username: string;
+  password: string;
+}
+export interface LoginCredentials extends AuthCredentialsBase {}
+export interface RegisterCredentials extends AuthCredentialsBase {
+  email: string;
+  repass: string;
+}
+//* Auth User Response Interface
+export interface AuthUserResponse {
+  token: string;
+  _id: string;
+  username: string;
+  email: string;
+  role: string;
 }
