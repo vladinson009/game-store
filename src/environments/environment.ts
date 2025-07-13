2d279f543bbd3e5ba16c2dd6d19a0a4c5fa17af1
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 19:16:53 +0200

    added environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  baseUrl: 'http://localhost:3001',
+  authEndpoints: {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout',
+  },
+};
