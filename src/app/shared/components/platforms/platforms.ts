1c4622d8faf51c1d8886ab94c5c73b6c99c9b3e9
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 19:17:30 +0200

    implemented platforms component

---
+++ src/app/shared/components/platforms/platforms.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-platforms',
+  imports: [MatCardModule],
+  templateUrl: './platforms.html',
+  styleUrl: './platforms.css',
+})
+export class Platforms {}
