524fc4ea0176208212afb4dbf900426f84d1deb8
Vladimir Gulev <vladinson009@icloud.com>
2025-07-15 22:49:09 +0200

    implemented logout and retrieve user methods

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,31 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 import type {
   AuthUserResponse,
   LoginCredentials,
   RegisterCredentials,
 } from '../../models/user';
+
 import { authEndpoints } from '../../shared/constants/apiEndpoints';
+import { tokenStorage } from '../../shared/constants/constants';
+import { UiService } from './ui.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private _userData = signal<AuthUserResponse | null>(null);
-
-  constructor(private http: HttpClient, private router: Router) {}
-
   public isLoggedIn = computed(() => !!this._userData());
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private uiService: UiService
+  ) {}
+
+  public get user() {
+    return this._userData;
+  }
+  private setLocalStorage(token: string) {
+    localStorage.setItem(tokenStorage, token);
+  }
+  public getLocalStorage() {
+    return localStorage.getItem(tokenStorage);
+  }
+  private clearLocalStorage() {
+    localStorage.removeItem(tokenStorage);
+  }
   public login(credentials: LoginCredentials): Observable<AuthUserResponse> {
     return this.http
       .post<AuthUserResponse>(authEndpoints.login, credentials)
       .pipe(
         tap((res) => {
           this._userData.set(res);
+          this.setLocalStorage(res.token);
           this.router.navigate(['/']);
         })
       );
@@ -38,16 +58,48 @@ export class AuthService {
       .pipe(
         tap((res) => {
           this._userData.set(res);
+          this.setLocalStorage(res.token);
           this.router.navigate(['/']);
         })
       );
   }
   public logout() {
-    this._userData.set(null);
-    this.router.navigate(['/']);
+    return this.http
+      .get<Observable<AuthUserResponse>>(authEndpoints.logout, {
+        headers: { 'x-authorization': this.user()?.token || 'magic-string' },
+      })
+      .pipe(
+        tap(() => {
+          this._userData.set(null);
+          this.clearLocalStorage();
+          this.router.navigate(['/']);
+        }),
+        catchError((error) => {
+          this.clearLocalStorage();
+          this.uiService.openSnackBar('Unauthorized Logout Request', '', 8000, {
+            redirectTo: '/',
+          });
+          return throwError(() => error);
+        })
+      );
   }
-
-  public get user() {
-    return this._userData;
+  public retrieveUser(): Observable<AuthUserResponse> {
+    const token = this.getLocalStorage() || 'magic-string';
+    return this.http
+      .get<AuthUserResponse>(authEndpoints.me, {
+        headers: { 'x-authorization': token },
+      })
+      .pipe(
+        tap((res) => {
+          this._userData.set(res);
+        }),
+        catchError((error) => {
+          this.clearLocalStorage();
+          this.uiService.openSnackBar('Invalid Session', 'Login Here', 8000, {
+            redirectTo: '/users/login',
+          });
+          return throwError(() => error);
+        })
+      );
   }
 }
