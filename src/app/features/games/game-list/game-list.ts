63abbf2ae8d3c457e161f18be2eb343b2a2b7aea
Vladimir Gulev <vladinson009@icloud.com>
2025-07-17 19:33:03 +0200

    init game list with static data

--- src/app/features/games/game-list/game-list.ts
+++ src/app/features/games/game-list/game-list.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import {
+  MatButton,
+  MatButtonModule,
+  MatIconButton,
+} from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/select';
+import { FocusInput } from '../../../shared/directives/focus-input.directive';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import fadeLeftAnimation from '../../../animations/fadeLeftAnimation';
+import slideAnimation from '../../../animations/slideAnimation';
+import carouselAnimation from '../../../animations/carouselAnimation';
 
 @Component({
   selector: 'app-game-list',
-  imports: [MatToolbar, MatToolbarRow, MatButtonModule],
+  imports: [
+    MatFormFieldModule,
+    MatIcon,
+    MatInput,
+    FocusInput,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './game-list.html',
   styleUrl: './game-list.css',
+  animations: [
+    fadeLeftAnimation(),
+    slideAnimation(1000, 'Y'),
+    carouselAnimation(1000),
+  ],
 })
 export class GameList {}
+//TODO: Paginator
