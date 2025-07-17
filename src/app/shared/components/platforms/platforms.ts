e03ec0a6dad94cdf2617261c54f0e04542eabd81
Vladimir Gulev <vladinson009@icloud.com>
2025-07-17 19:45:07 +0200

    moved dummy data to the class

--- src/app/shared/components/platforms/platforms.ts
+++ src/app/shared/components/platforms/platforms.ts
@@ -1,10 +1,125 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import fadeLeftAnimation from '../../../animations/fadeLeftAnimation';
 
 @Component({
   selector: 'app-platforms',
   imports: [MatCardModule],
   templateUrl: './platforms.html',
   styleUrl: './platforms.css',
+  animations: [fadeLeftAnimation()],
 })
-export class Platforms {}
+export class Platforms {
+  platforms = [
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+    {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
+      name: 'xBox',
+    },
+  ];
+}
