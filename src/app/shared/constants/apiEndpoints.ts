a6cdb28b554bc0771c25d0791cf3f0648b806973
Vladimir Gulev <vladinson009@icloud.com>
2025-07-19 20:54:29 +0200

    paths

--- src/app/shared/constants/apiEndpoints.ts
+++ src/app/shared/constants/apiEndpoints.ts
@@ -2,10 +2,37 @@ import { environment } from '../../../environments/environment';
 
 const baseUrl = environment.baseUrl;
 const usersUrl = environment.authEndpoints;
+const gamesUrl = environment.gameEndpoints;
+const platformsUrl = environment.platformEndpoints;
+const categoriesUrl = environment.categoryEndpoints;
 
 export const authEndpoints = {
   login: baseUrl + usersUrl.login,
   register: baseUrl + usersUrl.register,
   logout: baseUrl + usersUrl.logout,
   me: baseUrl + usersUrl.me,
+};
+export const gameEndpoints = {
+  getAll: baseUrl + gamesUrl.getAll,
+  create: baseUrl + gamesUrl.create,
+  edit: (id: string) => baseUrl + gamesUrl.edit(id),
+  delete: (id: string) => baseUrl + gamesUrl.delete(id),
+  getById: (id: string) => baseUrl + gamesUrl.getById(id),
+  like: (id: string) => baseUrl + gamesUrl.like(id),
+  unlike: (id: string) => baseUrl + gamesUrl.unlike(id),
+};
+export const platformEndpoints = {
+  getAll: baseUrl + platformsUrl.getAll,
+  create: baseUrl + platformsUrl.create,
+  edit: (id: string) => baseUrl + platformsUrl.edit(id),
+  delete: (id: string) => baseUrl + platformsUrl.delete(id),
+  getById: (id: string) => baseUrl + platformsUrl.getById(id),
+};
+
+export const categoryEndpoints = {
+  getAll: baseUrl + categoriesUrl.getAll,
+  create: baseUrl + categoriesUrl.create,
+  edit: (id: string) => baseUrl + categoriesUrl.edit(id),
+  delete: (id: string) => baseUrl + categoriesUrl.delete(id),
+  getById: (id: string) => baseUrl + categoriesUrl.getById(id),
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,4 +6,27 @@ export const environment = {
     logout: '/users/logout',
     me: '/users/me',
   },
+  gameEndpoints: {
+    getAll: '/games',
+    create: '/games/create',
+    edit: (id: string) => `/games/edit/${id}`,
+    delete: (id: string) => `/games/delete/${id}`,
+    getById: (id: string) => `/games/${id}`,
+    like: (id: string) => `/games/like/${id}`,
+    unlike: (id: string) => `/games/unlike/${id}`,
+  },
+  platformEndpoints: {
+    getAll: '/platforms',
+    create: '/platforms/create',
+    edit: (id: string) => `/platforms/edit/${id}`,
+    delete: (id: string) => `/platforms/delete/${id}`,
+    getById: (id: string) => `/platforms/${id}`,
+  },
+  categoryEndpoints: {
+    getAll: '/categories',
+    create: '/categories/create',
+    edit: (id: string) => `/categories/edit/${id}`,
+    delete: (id: string) => `/categories/delete/${id}`,
+    getById: (id: string) => `/categories/${id}`,
+  },
 };
