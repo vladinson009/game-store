5d74345a78381ad427ed1013a44806c6e25d8a62
Vladimir Gulev <vladinson009@icloud.com>
2025-07-19 23:30:33 +0200

    moved card in separated component

--- src/app/features/games/game-list/game-list.ts
+++ src/app/features/games/game-list/game-list.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
-import {
-  MatButton,
-  MatButtonModule,
-  MatIconButton,
-} from '@angular/material/button';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { MatFormField, MatLabel, MatSuffix } from '@angular/material/select';
 import { FocusInput } from '../../../shared/directives/focus-input.directive';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import fadeLeftAnimation from '../../../animations/fadeLeftAnimation';
 import slideAnimation from '../../../animations/slideAnimation';
 import carouselAnimation from '../../../animations/carouselAnimation';
+import { Paginator } from '../../../shared/components/paginator/paginator';
+import { GameService } from '../../../core/services/game.service';
+import {
+  GameCollectionSingleResponse,
+  GamesCollectionResponse,
+} from '../../../models/game';
+import { GameCard } from '../game-card/game-card';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-game-list',
@@ -23,6 +26,8 @@ import carouselAnimation from '../../../animations/carouselAnimation';
     FocusInput,
     MatCardModule,
     MatButtonModule,
+    Paginator,
+    GameCard,
   ],
   templateUrl: './game-list.html',
   styleUrl: './game-list.css',
@@ -32,5 +37,28 @@ import carouselAnimation from '../../../animations/carouselAnimation';
     carouselAnimation(1000),
   ],
 })
-export class GameList {}
-//TODO: Paginator
+export class GameList implements OnInit {
+  public games = signal<GameCollectionSingleResponse[] | null>(null);
+  public userId = signal<string | undefined>(undefined);
+  public limit = signal<number | null>(null);
+  public page = signal<number | null>(null);
+  public total = signal<number | null>(null);
+  public totalPage = signal<number | null>(null);
+
+  constructor(
+    private gameService: GameService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.gameService.getAll().subscribe((res: GamesCollectionResponse) => {
+      this.games.set(res.data);
+      this.limit.set(res.pagination.limit);
+      this.page.set(res.pagination.page);
+      this.total.set(res.pagination.total);
+      this.totalPage.set(res.pagination.totalPages);
+
+      this.userId.set(this.authService.user()?._id);
+    });
+  }
+}
