c333bd689a8e45c08054c83a197aac5885da1dcd
Vladimir Gulev <vladinson009@icloud.com>
2025-07-19 20:55:42 +0200

    removed carousel from home page

--- src/app/layout/home/home.ts
+++ src/app/layout/home/home.ts
@@ -1,10 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import carouselAnimation from '../../animations/carouselAnimation';
 import slideAnimation from '../../animations/slideAnimation';
 import { RouterLink } from '@angular/router';
 import { Platforms } from '../../shared/components/platforms/platforms';
+import { GameCollectionSingleResponse } from '../../models/game';
+import { GameService } from '../../core/services/game.service';
+import {
+  MatProgressSpinner,
+  MatProgressSpinnerModule,
+} from '@angular/material/progress-spinner';
 export interface HomeType {
   name: string;
   message: string;
@@ -13,68 +25,30 @@ export interface HomeType {
 }
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, CommonModule, RouterLink, Platforms],
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    RouterLink,
+    Platforms,
+    MatProgressSpinnerModule,
+    // MatProgressSpinner,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
   animations: [carouselAnimation(800), slideAnimation(800, 'Y')],
 })
-export class Home implements OnInit, OnDestroy {
+export class Home implements OnInit {
   private intervalId: null | number = null;
+  public isLoading = signal(true);
   public activeIndex = 0;
 
-  constructor(private cdr: ChangeDetectorRef) {}
-  ngOnInit() {
-    this.intervalId = setInterval(() => {
-      this.activeIndex = (this.activeIndex + 1) % this.testimonials.length;
-      this.cdr.markForCheck();
-    }, 10000);
-  }
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-  public testimonials = [
-    {
-      name: 'Counter Strike',
-      message: 'This is an amazing game',
-      imageUrl:
-        'https://images.unsplash.com/flagged/photo-1560177776-295b9cd779de?q=80&w=1004&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      role: 'Pet-Owner',
-    },
-    {
-      name: 'John Smith',
-      message: 'Very professional and caring staff. Highly recommend!',
-      imageUrl:
-        'https://plus.unsplash.com/premium_photo-1718652842683-8e7f623fbc1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fGJveGVyJTIwZG9nJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-      role: 'Dog-Trainer',
-    },
-    {
-      name: 'Jane Doe',
-      message:
-        ' This pet shop cha c ch lent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet sho life. Excellellent service! This pet sho life. Excellellent service! This pet sho life. Excellellent service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This pet sho life. Excellellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service!',
-      imageUrl:
-        'https://images.unsplash.com/photo-1599460093135-f881f382fc26?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGJveGVyJTIwZG9nJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-      role: 'Pet Owner',
-    },
-    {
-      name: 'John Smith',
-      message: 'Very professional and caring staff. Highly recommend!',
-      imageUrl:
-        'https://plus.unsplash.com/premium_photo-1718652842683-8e7f623fbc1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fGJveGVyJTIwZG9nJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-      role: 'Dog Trainer',
-    },
-  ];
+  public testimonial = signal<GameCollectionSingleResponse[] | []>([]);
 
-  public next() {
-    this.activeIndex = (this.activeIndex + 1) % this.testimonials.length;
-  }
-  public prev() {
-    this.activeIndex =
-      (this.activeIndex - 1 + this.testimonials.length) %
-      this.testimonials.length;
-  }
-  public setActive(index: number) {
-    this.activeIndex = index;
+  constructor(private gameService: GameService) {}
+  ngOnInit() {
+    this.gameService.getRecent().subscribe((res) => {
+      this.testimonial.set(res.data);
+      this.isLoading.set(false);
+    });
   }
 }
