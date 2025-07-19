1f1c1548eb6b31e892dc441244a87f93a4a6c15b
Vladimir Gulev <vladinson009@icloud.com>
2025-07-19 20:54:02 +0200

    request for total games on init and pass it to the footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  signal,
+  SimpleChanges,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { AuthService } from './core/services/auth.service';
+import { GameService } from './core/services/game.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +18,17 @@ import { AuthService } from './core/services/auth.service';
   styleUrl: './app.css',
 })
 export class App implements OnInit {
-  constructor(private authService: AuthService) {}
+  public totalGames = signal<number | null>(null);
+
+  constructor(
+    private authService: AuthService,
+    private gameService: GameService
+  ) {}
 
   ngOnInit(): void {
+    this.gameService.getRecent().subscribe((res) => {
+      this.totalGames.set(res.pagination.total);
+    });
     if (this.authService.getLocalStorage()) {
       this.authService.retrieveUser().subscribe();
     }
