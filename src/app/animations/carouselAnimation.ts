ab4436b7fa69188dd89872c70b0cf228c77c29ef
Vladimir Gulev <vladinson009@icloud.com>
2025-07-20 14:48:49 +0200

    bug fixed

--- src/app/animations/carouselAnimation.ts
+++ src/app/animations/carouselAnimation.ts
@@ -2,14 +2,14 @@ import { animate, style, transition, trigger } from '@angular/animations';
 
 export default function (ms: number) {
   return trigger('carouselAnimation', [
-    transition(':increment', [
+    transition(':enter', [
       style({ transform: 'translateX(100%)', opacity: 0 }),
       animate(
         `${ms}ms ease-out`,
         style({ transform: 'translateX(0)', opacity: 1 })
       ),
     ]),
-    transition(':decrement', [
+    transition(':leave', [
       style({ transform: 'translateX(-100%)', opacity: 0 }),
       animate(
         `${ms}ms ease-out`,
