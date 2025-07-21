3caa4ca6b84f3a4d507adc5d15ccd9b3c26a14e9
Vladimir Gulev <vladinson009@icloud.com>
2025-07-21 22:14:19 +0200

    fixed environments

--- src/app/shared/constants/apiEndpoints.ts
+++ src/app/shared/constants/apiEndpoints.ts
@@ -2,39 +2,42 @@ import { environment } from '../../../environments/environment';
 const imgBBApi =
   'https://api.imgbb.com/1/upload?key=c0f89984ba2349c19405d4fbd9c3e0c2';
 const baseUrl = environment.baseUrl;
-const usersUrl = environment.authEndpoints;
-const gamesUrl = environment.gameEndpoints;
-const platformsUrl = environment.platformEndpoints;
-const categoriesUrl = environment.categoryEndpoints;
+
+const USERS_ROOT = '/users';
+const GAMES_ROOT = '/games';
+const PLATFORMS_ROOT = '/platforms';
+const CATEGORIES_ROOT = '/categories';
 
 export const authEndpoints = {
-  login: baseUrl + usersUrl.login,
-  register: baseUrl + usersUrl.register,
-  logout: baseUrl + usersUrl.logout,
-  me: baseUrl + usersUrl.me,
+  login: baseUrl + USERS_ROOT + '/login',
+  register: baseUrl + USERS_ROOT + '/register',
+  logout: baseUrl + USERS_ROOT + '/logout',
+  me: baseUrl + USERS_ROOT + '/me',
 };
+
 export const gameEndpoints = {
-  getAll: baseUrl + gamesUrl.getAll,
-  create: baseUrl + gamesUrl.create,
-  edit: (id: string) => baseUrl + gamesUrl.edit(id),
-  delete: (id: string) => baseUrl + gamesUrl.delete(id),
-  getById: (id: string) => baseUrl + gamesUrl.getById(id),
-  like: (id: string) => baseUrl + gamesUrl.like(id),
-  unlike: (id: string) => baseUrl + gamesUrl.unlike(id),
+  getAll: baseUrl + GAMES_ROOT,
+  create: baseUrl + GAMES_ROOT + '/create',
+  edit: (id: string) => baseUrl + GAMES_ROOT + `/edit${id}`,
+  delete: (id: string) => baseUrl + GAMES_ROOT + `/delete${id}`,
+  getById: (id: string) => baseUrl + GAMES_ROOT + `/${id}`,
+  like: (id: string) => baseUrl + GAMES_ROOT + `/like${id}`,
+  unlike: (id: string) => baseUrl + GAMES_ROOT + `/unlike${id}`,
   uploadFileUrl: imgBBApi,
 };
+
 export const platformEndpoints = {
-  getAll: baseUrl + platformsUrl.getAll,
-  create: baseUrl + platformsUrl.create,
-  edit: (id: string) => baseUrl + platformsUrl.edit(id),
-  delete: (id: string) => baseUrl + platformsUrl.delete(id),
-  getById: (id: string) => baseUrl + platformsUrl.getById(id),
+  getAll: baseUrl + PLATFORMS_ROOT,
+  create: baseUrl + PLATFORMS_ROOT + '/create',
+  edit: (id: string) => baseUrl + PLATFORMS_ROOT + `/edit${id}`,
+  delete: (id: string) => baseUrl + PLATFORMS_ROOT + `/delete${id}`,
+  getById: (id: string) => baseUrl + PLATFORMS_ROOT + `/${id}`,
 };
 
 export const categoryEndpoints = {
-  getAll: baseUrl + categoriesUrl.getAll,
-  create: baseUrl + categoriesUrl.create,
-  edit: (id: string) => baseUrl + categoriesUrl.edit(id),
-  delete: (id: string) => baseUrl + categoriesUrl.delete(id),
-  getById: (id: string) => baseUrl + categoriesUrl.getById(id),
+  getAll: baseUrl + CATEGORIES_ROOT,
+  create: baseUrl + CATEGORIES_ROOT + '/create',
+  edit: (id: string) => baseUrl + CATEGORIES_ROOT + `/edit${id}`,
+  delete: (id: string) => baseUrl + CATEGORIES_ROOT + `/delete${id}`,
+  getById: (id: string) => baseUrl + CATEGORIES_ROOT + `/${id}`,
 };
