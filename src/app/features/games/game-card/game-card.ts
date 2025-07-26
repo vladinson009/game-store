c4ede013ecde62bea7f56c91acb5e2be427de807
Vladimir Gulev <vladinson009@icloud.com>
2025-07-26 16:31:50 +0300

    added variable in the template for game

--- src/app/features/games/game-card/game-card.ts
+++ src/app/features/games/game-card/game-card.ts
@@ -1,37 +1,26 @@
-import {
-  Component,
-  computed,
-  EventEmitter,
-  input,
-  OnInit,
-  output,
-  Signal,
-  signal,
-} from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { Author, GameCollectionSingleResponse } from '../../../models/game';
+import { GameCollectionSingleResponse } from '../../../models/game';
 import { RouterLink } from '@angular/router';
-import { PlatformsPipe } from '../../../shared/pipes/platforms.pipe';
+import { JoinArrayPipe } from '../../../shared/pipes/join-array';
 import { MatIcon } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthService } from '../../../core/services/auth.service';
-import { GameService } from '../../../core/services/game.service';
 
 @Component({
   selector: 'app-game-card',
-  imports: [MatCardModule, RouterLink, PlatformsPipe, MatIcon, MatButtonModule],
+  imports: [MatCardModule, RouterLink, JoinArrayPipe, MatIcon, MatButtonModule],
   templateUrl: './game-card.html',
   styleUrl: './game-card.css',
 })
 export class GameCard {
-  public game = input<GameCollectionSingleResponse>();
+  public gameSignal = input<GameCollectionSingleResponse>();
   public userId = input<string | undefined>(undefined);
   public like = output();
 
   public isLiked = computed(
     () =>
-      this.game()?.likes?.some((like) => like._id === this.userId()) ?? false
+      this.gameSignal()?.likes?.some((like) => like._id === this.userId()) ??
+      false
   );
   toggleLike() {
     this.like.emit();
