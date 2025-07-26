cabf16e68797ef76b1410af904c310c5d9f195be
Vladimir Gulev <vladinson009@icloud.com>
2025-07-26 16:30:14 +0300

    added route for game details card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Home } from './layout/home/home';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { CreateGame } from './features/games/create-game/create-game';
+import { GameDetails } from './features/games/game-details/game-details';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'games', component: GameList, pathMatch: 'full' },
   { path: 'games/create', component: CreateGame },
+  { path: 'games/details/:gameId', component: GameDetails },
   { path: 'users/login', component: Login },
   { path: 'users/register', component: Register },
 ];
