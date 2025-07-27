79d0c1efa711d171fc6c60d0199fa6fe063b4fc0
Vladimir Gulev <vladinson009@icloud.com>
2025-07-27 03:44:49 +0300

    dialog modal

---
+++ src/app/shared/components/dialog-modal/dialog-modal.ts
@@ -0,0 +1,38 @@
+import { Component, Inject, inject, input } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { GameCollectionSingleResponse } from '../../../models/game';
+import { AuthUserResponse } from '../../../models/user';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-dialog-modal',
+  imports: [MatDialogModule, MatButtonModule, MatIcon],
+  templateUrl: './dialog-modal.html',
+  styleUrl: './dialog-modal.css',
+})
+export class DialogModal {
+  public message: string | undefined;
+  public onAction: Function;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<DialogModal>
+  ) {
+    this.message = data.message;
+    this.onAction = data.onAction;
+  }
+
+  onDelete() {
+    this.onAction();
+    this.dialogRef.close();
+  }
+  onCancel() {
+    this.dialogRef.close();
+  }
+}
