01a52fb46be47b192b2d863c524938c55f5e30f0
Vladimir Gulev <vladinson009@icloud.com>
2025-07-30 15:36:12 +0300

    route guard for author only is fixed

--- src/app/core/guards/author-only-guard.ts
+++ src/app/core/guards/author-only-guard.ts
@@ -3,6 +3,8 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { GameService } from '../services/game.service';
 
+import { map } from 'rxjs/operators';
+
 export const authorOnlyGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const gameService = inject(GameService);
@@ -10,13 +12,13 @@ export const authorOnlyGuard: CanActivateFn = (route, state) => {
   const userId = authService.user()?._id;
   const gameId = route.paramMap.get('gameId')!;
 
-  let isAuthor: boolean = false;
-
-  gameService.getById(gameId).subscribe((game) => {
-    isAuthor = !!userId && game.author._id.toString() === userId;
-  });
-  if (isAuthor) {
-    return true;
-  }
-  return router.createUrlTree(['/not-found']);
+  return gameService.getById(gameId).pipe(
+    map((game) => {
+      const isAuthor = !!userId && game.author._id.toString() === userId;
+      if (isAuthor) {
+        return true;
+      }
+      return router.createUrlTree(['/not-found']);
+    })
+  );
 };
